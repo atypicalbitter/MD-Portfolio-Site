ee06588e7685c31922543690d7397bc93712dbaf
Matt Dudman <mattdudman95@googlemail.com>
2024-03-24 08:43:14 +0000

    Animation on 'matt' has been altered. Also footer nav has been aligned to the centre.

--- script.js
+++ script.js
@@ -12,3 +12,31 @@ window.onload = function() {
         nametitleElement.style.borderRight = 'none'; // Corrected the variable name
     });
 };
+window.addEventListener('load', function() {
+    var sectionText = document.querySelector('.section-text-p1');
+    var nameTitle = document.querySelector('.nametitle');
+    
+    sectionText.classList.add('fade-in');
+    nameTitle.classList.add('fade-in');
+});
+
+
+
+window.addEventListener('scroll', function() {
+    var aboutSection = document.getElementById('about');
+    var skillsSection = document.getElementById('skills');
+    var projectsSection = document.getElementById('projects');
+    
+    var sections = [aboutSection, skillsSection, projectsSection];
+    
+    sections.forEach(function(section) {
+        var title = section.querySelector('.title');
+        var sectionPosition = section.getBoundingClientRect().top;
+        var windowHeight = window.innerHeight;
+
+        if (sectionPosition < windowHeight / 1.5) { // Adjust the threshold as needed
+            title.classList.add('fade-in');
+        }
+    });
+});
+
